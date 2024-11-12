7408b2aad30152d4bfc7eeaedcbd7b00bbfb6300
Lorenzo <lorenzo1997chierisini@gmail.com>
2024-11-12 14:07:49 +0100

    Added custom controller

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace test_web_app_mvc_dotNET.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //GET: /HelloWorld
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: /HelloWorld/Welcome
+
+        [HttpGet("HelloWorld/Welcome/{name?}")]
+        public IActionResult Welcome(string name)
+        {
+            if(string.IsNullOrEmpty(name)) {
+                ViewData["Message"] = "Benvenuto, Ospite!";
+            } else {
+                ViewData["Message"] = "Benvenuto " + name;
+            }
+                return View();
+
+        }
+
+
+
+
+
+    }
+}
