858f277158819192d9dbc173724d3aa70f24879e
Lorenzo <lorenzo1997chierisini@gmail.com>
2024-11-12 11:42:58 +0100

    Aggiungere i file di progetto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace test_web_app_mvc_dotNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
